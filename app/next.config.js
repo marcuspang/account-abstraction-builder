56651bedbc79e2d7039a92ce9f154d93cf1f5077
Junx <junxiongong2@gmail.com>
2023-04-16 08:16:50 +0900

    add sorting logic

--- app/next.config.js
+++ app/next.config.js
@@ -1,25 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["via.placeholder.com"],
-  },
-  env: {
-    ALCHEMY_ID: process.env.ALCHEMY_ID,
-    WALLET_CONNECT_PROJECT_ID: process.env.WALLET_CONNECT_PROJECT_ID,
-  },
-  // webpack: (config, { isServer }) => {
-  //   if (!isServer) {
-  //     config.resolve.fallback = {
-  //       fs: false,
-  //       net: false,
-  //       tls: false,
-  //       http2: false,
-  //       dns: false,
-  //     };
-  //   }
-  //   return config;
-  // },
+	reactStrictMode: true,
+	images: {
+		domains: ["via.placeholder.com", "storage.googleapis.com"],
+	},
+	env: {
+		ALCHEMY_ID: process.env.ALCHEMY_ID,
+		WALLET_CONNECT_PROJECT_ID: process.env.WALLET_CONNECT_PROJECT_ID,
+	},
+	// webpack: (config, { isServer }) => {
+	//   if (!isServer) {
+	//     config.resolve.fallback = {
+	//       fs: false,
+	//       net: false,
+	//       tls: false,
+	//       http2: false,
+	//       dns: false,
+	//     };
+	//   }
+	//   return config;
+	// },
 };
 
 module.exports = nextConfig;
--- app/src/components/Plugins/PluginsSection.tsx
+++ app/src/components/Plugins/PluginsSection.tsx
@@ -1,85 +1,119 @@
 import { Dispatch, SetStateAction } from "react";
 import PluginCard from "./PluginCard";
+import groupBy from "lodash/groupBy";
+import Image from "next/image";
 
 interface PluginsSectionProps {
-  plugins: CodePlugin[];
-  setPlugins: Dispatch<SetStateAction<CodePlugin[]>>;
+	plugins: CodePlugin[];
+	setPlugins: Dispatch<SetStateAction<CodePlugin[]>>;
 }
 
 export const PLUGINS_DATA = [
-  {
-    displayName: "Send notification",
-    protocol: "Push",
-    id: 1,
-    description:
-      "Sends a push notification to the specified address whenever a transaction occurs",
-  },
-  {
-    displayName: "AAVE auto-lend",
-    protocol: "AAVE",
-    id: 2,
-    description:
-      "Immediately lends to the specified pool whenever a currency is deposited to the wallet",
-  },
-  {
-    displayName: "Social Recovery",
-    protocol: "Safe",
-    id: 3,
-    description:
-      "Enables you to recover your wallet through social media accounts",
-  },
-  {
-    displayName: "Make wallet soulbound to Worldcoin",
-    protocol: "Worldcoin",
-    id: 4,
-    description:
-      "Ensures that the wallet is only accessible to the specified Worldcoin account",
-    params: [
-      {
-        name: "test",
-        value: "test",
-      },
-      {
-        name: "test",
-        value: "test",
-      },
-    ],
-  },
+	{
+		displayName: "Send notification",
+		protocol: "Push",
+		src: "https://storage.googleapis.com/ethglobal-api-production/organizations%2F10a1v%2Flogo%2F1664802172170_aiOxYOJI_400x400.jpeg",
+		id: 1,
+		description:
+			"Sends a push notification to the specified address whenever a transaction occurs",
+	},
+	{
+		displayName: "AAVE auto-lend",
+		protocol: "AAVE",
+		src: "https://storage.googleapis.com/ethglobal-api-production/organizations%2Foiv32%2Flogo%2F1675803642879_EdjUdYhC_400x400.jpeg",
+		id: 2,
+		description:
+			"Immediately lends to the specified pool whenever a currency is deposited to the wallet",
+	},
+	{
+		displayName: "Social Recovery",
+		protocol: "Safe",
+		src: "https://storage.googleapis.com/ethglobal-api-production/organizations%2Fweaax%2Flogo%2F1667857487267_vRyTLmek_400x400.jpeg",
+		id: 3,
+		description:
+			"Enables you to recover your wallet through social media accounts",
+	},
+	{
+		displayName: "Make wallet soulbound to Worldcoin",
+		protocol: "Worldcoin",
+		src: "https://storage.googleapis.com/ethglobal-api-production/organizations%2F3zpxc%2Flogo%2F1664911741220_worldcoin.jpeg",
+		id: 4,
+		description:
+			"Ensures that the wallet is only accessible to the specified Worldcoin account",
+		params: [
+			{
+				name: "test",
+				value: "test",
+			},
+			{
+				name: "test",
+				value: "test",
+			},
+		],
+	},
 ];
 
+const logoPluginMap = {
+	Push: "https://storage.googleapis.com/ethglobal-api-production/organizations%2F10a1v%2Flogo%2F1664802172170_aiOxYOJI_400x400.jpeg",
+	AAVE: "https://storage.googleapis.com/ethglobal-api-production/organizations%2Foiv32%2Flogo%2F1675803642879_EdjUdYhC_400x400.jpeg",
+	Safe: "https://storage.googleapis.com/ethglobal-api-production/organizations%2Fweaax%2Flogo%2F1667857487267_vRyTLmek_400x400.jpeg",
+	Worldcoin:
+		"https://storage.googleapis.com/ethglobal-api-production/organizations%2F3zpxc%2Flogo%2F1664911741220_worldcoin.jpeg",
+};
+
 export interface CodePlugin {
-  displayName: string;
-  protocol: string;
-  id: number;
-  description: string;
+	displayName: string;
+	protocol: string;
+	id: number;
+	description: string;
 }
 
 const PluginsSection = ({ plugins, setPlugins }: PluginsSectionProps) => {
-  return (
-    <ul className="pb-12 space-y-2">
-      {PLUGINS_DATA.map((plugin, index) => (
-        <PluginCard
-          key={index}
-          {...plugin}
-          onClick={(id: number) =>
-            setPlugins((prev) => {
-              const currentPlugin = plugins.find((plugin) => plugin.id === id);
+	const pluginsByProtocol = groupBy(PLUGINS_DATA, "protocol");
 
-              if (currentPlugin === undefined) {
-                return [
-                  ...prev,
-                  PLUGINS_DATA.find((plugin) => plugin.id === id)!,
-                ];
-              }
-              const result = prev.filter((plugin) => plugin.id !== id);
-              return result;
-            })
-          }
-          currentPlugins={plugins}
-        />
-      ))}
-    </ul>
-  );
+	return (
+		<div className="pb-12 space-y-4">
+			{Object.entries(pluginsByProtocol).map(([protocol, protocolPlugins]) => (
+				<div key={protocol}>
+					<h2 className="text-2xl font-medium mb-3 flex gap-2">
+						<Image
+							alt={protocol}
+							src={logoPluginMap[protocol]}
+							width={32}
+							height={32}
+							className=" rounded-full"
+						/>
+						{protocol}
+					</h2>
+					<ul className="space-y-2">
+						{protocolPlugins.map((plugin) => (
+							<PluginCard
+								key={plugin.id}
+								{...plugin}
+								onClick={(id: number) =>
+									setPlugins((prev) => {
+										const currentPlugin = plugins.find(
+											(plugin) => plugin.id === id
+										);
+
+										if (currentPlugin === undefined) {
+											return [
+												...prev,
+												PLUGINS_DATA.find((plugin) => plugin.id === id)!,
+											];
+										}
+										const result = prev.filter((plugin) => plugin.id !== id);
+										return result;
+									})
+								}
+								currentPlugins={plugins}
+							/>
+						))}
+					</ul>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default PluginsSection;
